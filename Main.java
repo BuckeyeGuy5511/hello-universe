b169a5bba90fdf6a5a2056a14abc9ce94d9f0797
Tyler Henry <91331801+BuckeyeGuy5511@users.noreply.github.com>
2021-09-28 14:48:22 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package edu.cscc;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+   private static Scanner input = new Scanner (System.in);
+
+    public static void main(String[] args) {
+        //Starting menu
+        System.out.println("Welcome to Henry Online Bank!\r\nWhat would you like to do today?\r\n \r\n" +
+                "1. Log in\r\n" +
+                "2. Create an Account with us");
+
+
+        int userInputStart;
+        userInputStart = userInput(input.nextInt());
+
+        if (userInputStart == 1);
+
+        else if (userInputStart == 2);
+
+        else
+
+        }
+        static int userInput (int userPath){
+            return userPath;
+    }
+}
